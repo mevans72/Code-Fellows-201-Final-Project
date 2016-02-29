4295fa3bad85f76ffa6d46e85c6d27eb2d3febac
bonecrushereb <bonecrushereb@gmail.com>
2016-02-29 13:24:16 -0800

    started app.js for questionaire

---
+++ app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var complianceAudit = function(questionName, questAnswer){
+  this.questionName = questionName;
+  this.questAnswer = [];
+  this.recommendation =recommendation;
+  this.auditResults = [];
+};
+
+var question1 = new complianceAudit('question1',questAnswer);
+var question2 = new complianceAudit('question2', questAnswer);
+var question3 = new complianceAudit('question3', questAnwer);
+var question1 = new complianceAudit('question4', questAnwer);
+var question2 = new complianceAudit('question5', questAnwer);
+var question3 = new complianceAudit('question6', questAnwer);
+var question1 = new complianceAudit('question7', questAnwer);
+var question2 = new complianceAudit('question8', questAnwer);
+var question3 = new complianceAudit('question9', questAnwer);
+var question1 = new complianceAudit('question10', questAnwer);
+var question1 = new complianceAudit('question11', questAnwer);
+var question2 = new complianceAudit('question12', questAnwer);
+var question3 = new complianceAudit('question13', questAnwer);
+var question1 = new complianceAudit('question14', questAnwer);
+var question2 = new complianceAudit('question15', questAnwer);
+var question3 = new complianceAudit('question16', questAnwer);
+var question1 = new complianceAudit('question17', questAnwer);
+var question2 = new complianceAudit('question18', questAnwer);
+var question3 = new complianceAudit('question19', questAnwer);
+var question1 = new complianceAudit('question20', questAnwer);
